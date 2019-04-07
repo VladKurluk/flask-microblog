ae6a24ed2ec4cb6d63846a82673987c086dca238
VladKurluk <vlad11kurluk89@gmail.com>
2019-04-07 18:06:58 +0300

    Create form for User sign in

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +22,8 @@ def index():
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
